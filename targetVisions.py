63a6c50dd7827abfdd27f72203f90ebb521ad10c
Richard L <rhlin2004@gmail.com>
2022-01-14 18:08:36 -0500

    corrected hsv values

--- targetVisions.py
+++ targetVisions.py
@@ -16,34 +16,39 @@ BALL_RADIUS             = 3.5
 MIN_AREA_CONTOUR        = 50.0
 
 # filter = {"hue": [57, 111], "sat":[148, 255], "val": [255, 255]}
-hue = [57, 111]
-sat = [148, 255]
-val = [255, 255]
+# hue = [57, 111]
+# sat = [148, 255]
+# val = [255, 255]
+
+hue = [73, 112]
+sat = [108, 174]
+val = [162, 255]
 
 cap = cv2.VideoCapture(0)
 
 while True:
-    # try:
-    isTrue, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, (hue[0], sat[0], val[0]), (hue[1], sat[1], val[1]))
+    try:
+        isTrue, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, (hue[0], sat[0], val[0]), (hue[1], sat[1], val[1]))
 
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    tempCounters = []
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area < MIN_AREA_CONTOUR:
-            continue
-        tempCounters.append(contour)
-    contours = tempCounters
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        tempCounters = []
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area < MIN_AREA_CONTOUR:
+                continue
+            tempCounters.append(contour)
+        contours = tempCounters
 
-    convexHulls = [cv2.convexHull(contour) for contour in contours]
-    cv2.drawContours(frame, convexHulls, -1, (0, 255, 0), 1)
+        convexHulls = [cv2.convexHull(contour) for contour in contours]
+        cv2.drawContours(frame, convexHulls, -1, (0, 0, 255), 1)
 
-    cv2.imshow("frame", frame)
+        cv2.imshow("mask", mask)
+        cv2.imshow("frame", frame)
 
-    if cv2.waitKey(20) & 0xFF == ord("d"):
-        print("done")
-        break
-    # except:
-    #     pass
+        if cv2.waitKey(20) & 0xFF == ord("d"):
+            print("done")
+            break
+    except:
+        pass
