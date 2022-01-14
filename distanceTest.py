12c4c9531395fac070442e63c3535748904ad30e
lilrichie <59586372+lilrichie@users.noreply.github.com>
2022-01-13 23:53:47 -0500

    added to findFocalLength and created distanceTest

---
+++ distanceTest.py
@@ -0,0 +1,29 @@
+import findFocalLength
+import cv2 as cv
+import numpy as np
+import imutils
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("error")
+    quit()
+
+while True:
+    isTrue, frame = cap.read()
+    if isTrue:
+        marker = findFocalLength.findMarker(frame)
+        # focalLength = 1502.5880859375
+        focalLength = 800
+        inches = findFocalLength.getDistance(focalLength, findFocalLength.WIDTH, marker[1][0])
+        box = cv.cv.BoxPoints(marker) if imutils.is_cv2() else cv.boxPoints(marker)
+        box = np.int0(box)
+        cv.drawContours(frame, [box], -1, (0, 255, 0), 2)
+        cv.putText(frame, "%.2fin" % inches, (frame.shape[1] - 275, frame.shape[0] - 20), cv.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+        cv.imshow("frame", frame)
+        if cv.waitKey(20) & 0xFF == ord("d"):
+            print("done")
+            break
+    else:
+        print("done")
+        break
--- findFocalLength.py
+++ findFocalLength.py
@@ -5,10 +5,10 @@ from imutils import paths
 
 def findMarker(img):
     # grayscale, blur, detect edges
-    gray = cv.cvtColor(img, cv.COLOR_BGR2Gray)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray, (5, 5,), 0)
     edges = cv.Canny(blur, 35, 125)
-
+    cv.imshow("edges", edges)
     # assume largest counter is tape
     cnts = cv.findContours(edges.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
@@ -16,3 +16,24 @@ def findMarker(img):
     
     # compute the bounding box of the of the paper region and return it
     return cv.minAreaRect(c)
+
+def getDistance(focalLength, width, pixelWidth):
+    return (focalLength * width) / pixelWidth
+
+WIDTH = 5.0
+DISTANCE = 12.0
+
+if __name__ == "__main__":
+    img = cv.imread("tape8.jpg")
+    marker = findMarker(img)
+    print("marker: ", marker[1][0])
+    focalLength = (marker[1][0] * DISTANCE) / WIDTH
+    inches = getDistance(focalLength, WIDTH, marker[1][0])
+    print("focalLength: ", focalLength)
+    box = cv.cv.BoxPoints(marker) if imutils.is_cv2() else cv.boxPoints(marker)
+    box = np.int0(box)
+    cv.drawContours(img, [box], -1, (0, 255, 0), 2)
+    cv.putText(img, "%.2fin" % inches, (img.shape[1] - 200, img.shape[0] - 40), cv.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+    cv.imshow("img", img)
+
+    cv.waitKey(0)
