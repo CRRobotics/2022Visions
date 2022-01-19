da538301f59939626aef33e9458c1ce1ba7655a8
Richard L <rhlin2004@gmail.com>
2022-01-18 21:22:27 -0500

    fixed getAngle function

--- library/functions.py
+++ library/functions.py
@@ -3,7 +3,12 @@ from re import X
 from tkinter import W
 import cv2
 import numpy
-import library.constants as constants
+
+from library.constants import RADIANS_PER_PIXEL_X
+if __name__ == "__main__":
+    import constants
+else:
+    import library.constants as constants
 
 
 
@@ -91,16 +96,29 @@ def getVertex(a,b,c):
     "y = ax^2+bx+c"
     return ((-b / (2 * a)), (((4 * a * c) - (b * b)) / (4 * a)))
 
-def getAngleVertical(img, coordinate:tuple):
+# gets the angle based on the fov, orientation, and coordinate of the parabola representing the target
+# @param img The image or frame to use
+# @param orientation The orientation of the angle (0 gets horizontal angle to the coordinate, 1 gets vertical angle to the tape)
+# NOTE: vertical angle is only accurate if the x-coordinate of the center coordinate is in the middle of the frame
+# @param coordinate The coordinate of the center of the tape or parabola
+
+def getAngle(img, orientation:int, coordinate:tuple):
     h, w, c = img.shape
-    fov = 45
     cX = coordinate[0]
     cY = coordinate[1]
 
-    centerPixel = (int(h/2), int(w/2))
-    distanceFromCenter = (math.sqrt(((cX-centerPixel[0])**2)+((cY-centerPixel[1])**2)))
-    pixelRepresent = fov/h
-    return int(pixelRepresent*distanceFromCenter)
+    # centerPixel = (int(h/2), int(w/2))
+    # distanceFromCenter = (math.sqrt(((cX - centerPixel[0]) **2 ) + ((cY - centerPixel[1]) ** 2)))
+    centerPixel = (int(w / 2), int(h / 2))
+    if orientation == 0:
+        distanceFromCenter = abs(centerPixel[0] - cX)
+        angle = constants.RADIANS_PER_PIXEL_X * distanceFromCenter
+    elif orientation == 1:
+        distanceFromCenter = abs(centerPixel[1] - cY)
+        angle = constants.RADIANS_PER_PIXEL_Y * distanceFromCenter
+    return angleToDegrees(angle)
+    # pixelRepresent = fov/(math.sqrt(h ** 2 + w ** 2))
+    # return int(pixelRepresent * distanceFromCenter)
 
 #def getPixelRepresent(img, pixel):
 #    h, w, c = img.shape
@@ -108,4 +126,7 @@ def getAngleVertical(img, coordinate:tuple):
 #    pixelRepresent = fov/(math.sqrt(h**2 + w**2))
 #    return pixelRepresent
 def angleToRadians(degrees):
-    return degrees*(math.pi/180)
+    return degrees*(math.pi / 180)
+
+def angleToDegrees(radians):
+    return (radians / math.pi) * 180
