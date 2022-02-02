03277eb0d520a770b3b24d77f1befb8c07118032
Richard L <rhlin2004@gmail.com>
2022-02-01 20:15:24 -0500

    started opticalToGround

--- webcam/library/functions.py
+++ webcam/library/functions.py
@@ -90,6 +90,11 @@ def getVertex(a, b, c):
     "y = ax^2+bx+c"
     return (int(-b / (2 * a)), int(((4 * a * c) - (b * b)) / (4 * a)))
 
+# converts the horizontal angle relative to the optical axis to the horizontal angle relative to the ground
+# to be implemented
+def opticalToGround(angle):
+    return
+
 # gets the angle based on the fov, orientation, and coordinate of the parabola representing the target
 # @param img The image or frame to use
 # @param orientation The orientation of the angle (0 gets horizontal angle to the coordinate, 1 gets vertical angle to the tape)
