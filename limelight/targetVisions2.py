7cfb4918a06e56bf35f918df5bd0af7b877b12a2
Richard Lin <59586372+r1chardlin@users.noreply.github.com>
2022-02-22 21:08:41 -0500

    changed threshold

--- limelight/targetVisions2.py
+++ limelight/targetVisions2.py
@@ -10,7 +10,7 @@ count = 0
 
 # CONSTANTS
 
-CAMERA_EXPOSURE           = 11 # (0.1 ms) to be set in "input" tab in limelight
+CAMERA_EXPOSURE           = 16 # 11 # (0.1 ms) to be set in "input" tab in limelight
 
 # HEIGHT_OF_CAMERA        = 29.0   # actual
 # HEIGHT_OF_TARGET        = 104.0  # actual
@@ -91,7 +91,7 @@ def binarizeSubt(img):
     # print(type(blue[0][0]), type(green[0][0]), type(red[0][0]))
     diff = cv2.subtract(green, red)
     # ret, binImage = cv2.threshold(diff, 0, 255, cv2.THRESH_OTSU)
-    ret, binImage = cv2.threshold(diff, 75, 255, cv2.THRESH_BINARY)
+    ret, binImage = cv2.threshold(diff, 79, 255, cv2.THRESH_BINARY)
     # ret, binImage = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     # binImage = cv2.cvtColor(binImage, cv2.COLOR_BGR2GRAY) # idk if we need this
     return binImage
